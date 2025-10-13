95087ccaea1d71ab796d0c411e4b45d439eea5d1
b3e3e3 <3182953+b3e3e3@users.noreply.github.com>
2025-10-13 02:15:04 -0700

    moved block placing logic to observer

--- observers/block_observer.gd
+++ observers/block_observer.gd
@@ -2,8 +2,34 @@ class_name BlockObserver
 extends Observer
 
 func watch() -> Resource:
-    return BlockTransformComponent
+	return BlockTransformComponent
 
 func on_component_added(entity: Entity, component: Resource) -> void:
-    print("Syncing position %s -> %s" % [component.position, entity.position])
-    component.position = entity.position
+	print("Syncing position %s -> %s" % [component.position, entity.position])
+	
+	component.position = entity.position
+	Global.current_terrain.get_voxel_tool().set_voxel_metadata(component.position, entity)
+
+
+func _on_player_block_placed(where: Vector3i, what: int) -> void:
+	var tarnation := [4, 5]
+	if not what in tarnation: return
+	
+	var block := MachineEntity.new()
+	block.position = where
+
+	ECS.world.add_entity(block)
+	print("Created machine entity at %s" % [where])
+
+func _on_player_block_broken(where: Vector3i, what: int) -> void:
+	var block := Global.current_terrain.get_voxel_tool().get_voxel_metadata(where) as BlockEntity
+	if not block:
+		push_warning("Couldn't find destroyed block entity :/")
+		return
+
+	var transform := block.get_component(BlockTransformComponent) as BlockTransformComponent
+	print('Retrieved matching block metadata from %s? %s (%s, %s)' % [where, block != null and transform.position == where, transform.position, where])
+
+	block.queue_free()
+	ECS.world.remove_entity(block)
+	
--- world.gd
+++ world.gd
@@ -13,33 +13,11 @@ func _ready() -> void:
 
 	ECS.world = world
 
-	# ecs systems
-	ECS.world.add_system(MachineSystem.new())
-
-	# ecs observers
-	ECS.world.add_observer(BlockObserver.new())
-
 func _process(delta):
 	if ECS.world:
 		ECS.process(delta)
 
-func _on_player_block_placed(where: Vector3i, what: int) -> void:
-	# TODO: associate block IDs with an entity class, maybe some kind of lookup table of references that can autofill the blocklibrary?
-	var tarnation := [4, 5]
-	if not what in tarnation: return
-
-	# create entity?
-	var block := MachineEntity.new()
-	block.position = where
-	# var block_tsf := block.get_component(BlockTransformComponent) as BlockTransformComponent
-
-	add_child(block)
-
-	terrain.get_voxel_tool().set_voxel_metadata(where, block)
-
-	ECS.world.add_entity(block)
-	print("Created block entity at %s" % [where])
-	
+# func _on_player_block_placed(where: Vector3i, what: int) -> void:
 	# TODO: bug found in GECS?
 	# Entity.on_ready() does NOT fire after waiting for component_added
 	# all the stuff like add child, set metadata, etc., were all happening AFTER the await before
@@ -48,15 +26,3 @@ func _on_player_block_placed(where: Vector3i, what: int) -> void:
 
 	# block_tsf.position = where
 
-
-func _on_player_block_broken(where: Vector3i, what: int) -> void:
-	var block := terrain.get_voxel_tool().get_voxel_metadata(where) as BlockEntity
-	if not block:
-		push_warning("Couldn't find destroyed block entity :/")
-		return
-	
-	var component := block.get_component(BlockTransformComponent) as BlockTransformComponent
-	print('Retrieved matching block metadata from %s? %s (%s, %s)' % [where, block != null and component.position == where, component.position, where])
-
-	block.queue_free()
-	ECS.world.remove_entity(block)
