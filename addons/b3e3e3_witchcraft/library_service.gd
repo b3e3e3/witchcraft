6058ec9fc9dc4fb7dfa62ccd02309f09d989d476
b3e3e3 <3182953+b3e3e3@users.noreply.github.com>
2025-10-19 22:40:21 -0700

    refactor, broke shit, fixed shit, unexplained errors now?

---
+++ addons/b3e3e3_witchcraft/library_service.gd
@@ -0,0 +1,70 @@
+@tool
+extends Node
+
+func update_block_library(terrain: VoxelTerrain, atlas_path: String = "res://atlas.png"):
+	var generator := terrain.generator
+	var mesher := terrain.mesher
+
+	terrain.material_override.albedo_texture = ResourceLoader.load(atlas_path)
+
+	print("Got mesher and generator")
+	if mesher is VoxelMesherBlocky:
+		var voxel_mesher := mesher as VoxelMesherBlocky
+		var library := voxel_mesher.library as VoxelBlockyLibrary
+		var blocks := AtlasService.get_all_blocks()
+
+		library.models = []
+
+		print("Got %s block(s)" % blocks.size())
+
+		var library_json := {
+			"version": 1,
+			"blocks": []
+		}
+
+		for block in blocks:
+			var model = block.model
+			if not model: continue
+
+			library_json["blocks"].append({
+				"uuid": block.uuid,
+				"resource_path": model.resource_path,
+			})
+
+			if model is VoxelBlockyModelCube:
+				var sides: Dictionary[Global.BlockSide, StringName] = {
+					Global.BlockSide.LEFT: "tile_left",
+					Global.BlockSide.RIGHT: "tile_right",
+					Global.BlockSide.TOP: "tile_top",
+					Global.BlockSide.BOTTOM: "tile_bottom",
+					Global.BlockSide.FRONT: "tile_front",
+					Global.BlockSide.BACK: "tile_back"
+				}
+
+				var front := AtlasService.get_block_side_texture(block.uuid, Global.BlockSide.FRONT)
+				if block.uuid == "dirt":
+					print("FRONT ", front)
+				if front != {}:
+					for side in sides:
+						model[sides[side]].x = front["position"][0]
+						model[sides[side]].y = front["position"][1]
+
+						if block.textures.has(side):
+							var tex := AtlasService.get_block_side_texture(block.uuid, side)
+							model[sides[side]].x = tex["position"][0]
+							model[sides[side]].y = tex["position"][1]
+
+				model.atlas_size_in_tiles = AtlasService.get_size_in_tiles()
+				print("Size in tiles:", model.atlas_size_in_tiles)
+
+			library.add_model(model)
+
+		var file = FileAccess.open("res://library.json", FileAccess.WRITE)
+		if file:
+			var editor_filesystem := EditorInterface.get_resource_filesystem()
+			if file.store_string(JSON.stringify(library_json)):
+				editor_filesystem.reimport_files(["res://library.json"])
+				print("Reimported library json")
+			file.close()
+		else:
+			print("Error opening file for writing.")
--- addons/b3e3e3_witchcraft/plugin.gd
+++ addons/b3e3e3_witchcraft/plugin.gd
@@ -3,14 +3,29 @@ extends EditorPlugin
 
 var block_plugin
 
+var plugin_path: String = get_script().resource_path.get_base_dir()
+
 func _enable_plugin() -> void:
-	add_autoload_singleton("AtlasService", "res://addons/b3e3e3_witchcraft/atlas_service.gd")
+	print(plugin_path)
+
+	var atlas_service := load("%s/atlas_service.gd" % plugin_path)
+	add_autoload_singleton("AtlasService", "%s/atlas_service.gd" % plugin_path)
+	add_tool_menu_item("Stitch block atlas", Callable(atlas_service, "stitch_atlas"))
+
+	var library_service := load("%s/library_service.gd" % plugin_path)
+	add_autoload_singleton("LibraryService", "%s/library_service.gd" % plugin_path)
+	add_tool_menu_item("Build block library", Callable(library_service, "update_block_library"))
+
+func _disable_plugin() -> void:
+	remove_autoload_singleton("AtlasService")
+	remove_autoload_singleton("LibraryService")
 
 func _enter_tree() -> void:
-	block_plugin = preload("res://addons/b3e3e3_witchcraft/block_plugin.gd")
+	block_plugin = load("%s/block_plugin.gd" % plugin_path)
 	add_inspector_plugin(block_plugin.new())
 
-	add_tool_menu_item("Stitch block atlas", Callable(block_plugin, "stitch_atlas"))
-
 func _exit_tree() -> void:
+	remove_tool_menu_item("Stitch block atlas")
+	remove_tool_menu_item("Build block library")
+
 	remove_inspector_plugin(block_plugin)
